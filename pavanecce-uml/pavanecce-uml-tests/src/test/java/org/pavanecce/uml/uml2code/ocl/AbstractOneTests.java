17447553b6cfdfd971da857e3932de869bce4b7d
Ampie Barnard <ampieb@gmail.com>
2014-05-03 16:40:01 +0200

    Some more OCL tests

--- pavanecce-uml/pavanecce-uml-tests/src/test/java/org/pavanecce/uml/uml2code/ocl/AbstractOneTests.java
+++ pavanecce-uml/pavanecce-uml-tests/src/test/java/org/pavanecce/uml/uml2code/ocl/AbstractOneTests.java
@@ -1,7 +1,5 @@
 package org.pavanecce.uml.uml2code.ocl;
 
-import org.eclipse.emf.common.util.BasicEList;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.uml2.uml.OpaqueExpression;
 import org.eclipse.uml2.uml.Operation;
 import org.eclipse.uml2.uml.Parameter;
@@ -10,16 +8,17 @@ import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.junit.Test;
 
-public abstract class AbstractSelectTests extends AbstractOclTest {
+public abstract class AbstractOneTests extends AbstractOclTest {
 	protected static void addOcl() {
-		Operation find = example.getConstructionCase().createOwnedOperation("findRoomPlan", list("nameToFind"), list((Type) example.getType("String")));
-		Parameter result = find.createOwnedParameter("result", example.getRoomPlan());
+		Operation find = example.getConstructionCase().createOwnedOperation("existsOneRoomPlan", list("nameToFind"), list((Type) example.getType("String")));
+		Parameter result = find.createOwnedParameter("result", example.getType("Boolean"));
 		result.setDirection(ParameterDirectionKind.RETURN_LITERAL);
-		result.setUpper(-1);
+		result.setUpper(1);
 		OpaqueExpression ocl = (OpaqueExpression) find.createBodyCondition("body").createSpecification("spec", example.getRoomPlan(), UMLPackage.eINSTANCE.getOpaqueExpression());
 		ocl.getLanguages().add("ocl");
-		ocl.getBodies().add("housePlan.roomPlan->select(name=nameToFind)");
+		ocl.getBodies().add("housePlan.roomPlan->one(rp|rp.name=nameToFind)");
 	}
+
 
 
 	@Test
@@ -27,17 +26,16 @@ public abstract class AbstractSelectTests extends AbstractOclTest {
 		eval("var constructionCase=new ConstructionCase();");
 		eval("var housePlan=new HousePlan();");
 		eval("constructionCase.setHousePlan(housePlan);");
-		eval("var wallPlan1=new WallPlan();");
-		eval("var wallPlan2=new WallPlan();");
-		eval("housePlan.getWallPlan().add(wallPlan1);");
-		eval("housePlan.getWallPlan().add(wallPlan2);");
 		eval("var roomPlan1=new RoomPlan();");
 		eval("roomPlan1.setName('roomPlan1');");
 		eval("var roomPlan2=new RoomPlan();");
 		eval("roomPlan2.setName('roomPlan2');");
+		eval("var roomPlan3=new RoomPlan();");
+		eval("roomPlan3.setName('roomPlan2');");
 		eval("housePlan.getRoomPlan().add(roomPlan1);");
 		eval("housePlan.getRoomPlan().add(roomPlan2);");
-		eval("var found=constructionCase.findRoomPlan('roomPlan1');");
-		assertEquals(1, eval("found.size();"));
+		eval("housePlan.getRoomPlan().add(roomPlan3);");
+		assertEquals(true, eval("constructionCase.existsOneRoomPlan('roomPlan1');"));
+		assertEquals(false, eval("constructionCase.existsOneRoomPlan('roomPlan2');"));
 	}
 }
---
+++ pavanecce-uml/pavanecce-uml-tests/src/test/java/org/pavanecce/uml/uml2code/ocl/JavascriptOneTests.java
@@ -0,0 +1,34 @@
+package org.pavanecce.uml.uml2code.ocl;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Set;
+
+import javax.script.ScriptException;
+
+import org.junit.BeforeClass;
+import org.pavanecce.common.util.ConstructionCaseExample;
+import org.pavanecce.uml.uml2code.javascript.JavaScriptGenerator;
+
+public class JavascriptOneTests extends AbstractOneTests {
+	@BeforeClass
+	public static void before() throws Exception {
+		example = new ConstructionCaseExample("JavascriptOne");
+		addOcl();
+		example.generateCode(new JavaScriptGenerator());
+		example.initScriptingEngine();
+		Set<File> newFiles = example.getTextFileGenerator().getNewFiles();
+		evaluateResource("underscore.js");
+		evaluateResource("backbone.js");
+
+		for (File file : newFiles) {
+			System.out.println(file);
+			example.getJavaScriptEngine().eval(new FileReader(file));
+		}
+	}
+
+	protected static void evaluateResource(String name2) throws ScriptException {
+		example.getJavaScriptEngine().eval(new InputStreamReader(JavascriptOneTests.class.getClassLoader().getResourceAsStream(name2)));
+	}
+}
